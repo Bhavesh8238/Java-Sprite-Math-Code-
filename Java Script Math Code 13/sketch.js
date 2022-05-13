f3b399ee911bf414d3d7a786ab8b46f18091b35b
Bhavesh8238 <97882765+Bhavesh8238@users.noreply.github.com>
2022-05-13 10:38:12 +0530

    Add files via upload

---
+++ Java Script Math Code 13/sketch.js
@@ -0,0 +1,55 @@
+var building , buildingImg
+var ground,groundImg
+
+function preload () {
+
+  groundImg = loadImage("ground2.png");
+  buildingImg  = loadImage("buildings.png");
+}
+
+function setup () {
+  createCanvas(600,200);
+  
+  
+  
+
+  ground  = createSprite(100,200,50,50)
+  ground.addImage("ground",groundImg)
+  ground.velocityX = -2
+   
+}
+
+function draw () {
+  
+  background ("white");
+
+
+  console.log(ground.x)
+  if (ground.x >-100){
+      ground.x  = 100
+  }
+
+  spawnBuildings()
+   drawSprites()
+}
+  
+
+
+function spawnBuildings(){
+   if (frameCount % 60 === 0){
+  
+  building  = createSprite(550,150,50,50)
+  building.addImage ("building",buildingImg)
+  building.scale = 0.1
+  building.velocityX  = -2
+
+
+  console.log (building.x)
+  building.x = Math.log(random(200,600))
+
+ 
+}
+
+
+}
+
